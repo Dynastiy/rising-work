ce814e40a8d7ffa7674d86c708f4070319371c01
Dynastiy <emmanuel00michael@gmail.com>
2022-05-30 07:49:39 +0100

    fixed services page for redirect

--- src/router/index.js
+++ src/router/index.js
@@ -36,6 +36,15 @@ const routes = [{
                 import ( /* webpackChunkName: "about" */ '../views/pages/homePage/_id.vue')
         },
         {
+            path: '/services/',
+            name: 'product-details',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () =>
+                import ( /* webpackChunkName: "about" */ '../views/pages/homePage/serviceId.vue')
+        },
+        {
             path: '/cart/checkout',
             name: 'checkout',
             // route level code-splitting
---
+++ src/views/pages/homePage/serviceId.vue
@@ -0,0 +1,326 @@
+<template>
+    <div class="pb-3">
+        <div class=" shadow-lg py-3">
+           <div class="container">
+               <span> Home </span>
+               <span> <IconComponent icon="uit:angle-double-right"/> </span>
+               <span class="text-capitalize"> {{ product.name }} </span>
+           </div>
+        </div>
+        <div class="container mt-4">
+            <div class="single_product">
+                <!-- <h1 class="text-capitalize"> {{ product.name }} </h1>
+                <p> <span v-if="rating.avg_rating !== null ">Rating {{ rating.avg_rating }}</span> {{ rating.total_reviews}}  reviews</p> -->
+                <section>
+                    <div class="body--content">
+                        <div>
+                            <Gallery :dataObj="dataObj"/>
+                        </div>
+                       <div class="">
+                            <div class="item--details">
+                                <div class="">
+                                <h1 class="font-weight-bold text-uppercase">
+                                    {{ product.name }}
+                                </h1>
+                                <p class="small font-weight-bold text-uppercase" style="color:var(--primary-color)" v-if="product.category"><span class="text-dark">Category:</span> {{ product.category.category_name }} </p>
+                            </div>
+                            <div class="mt-3">
+                                
+                                <div class="select--plan mb-3">
+                                    <label for="" class="m-0 d-block text-capitalize text-dark"> Select Plan to add to Cart </label>
+                                    <hr class="">
+                                   <div class="d-lg-flex align-items-center" style="gap:20px">
+                                       <div class="plan--selector" id="myDIV" v-for="plan in plansObj" :key="plan.id" >
+                                            <button class="nav--item" :class="{ active: (isActive === plan.id) }" @click="selectPlan(plan)">{{ plan.name}}</button>
+                                        </div>
+                                   </div>
+                                   <hr class="">
+
+                                   <div>
+                                       <h5>Plan Details</h5>
+                                   </div>
+                                </div>
+                                <div>
+                                    <label  for="" class="m-0 text-dark">Select Additional Features</label>
+                                    <div class="d-flex align-items-center mb-2" style="gap:10px" v-for="feature in product.features" :key="feature.id">
+                                        <input type="checkbox" :id="feature.id" :value="feature" v-model="cartItem" @change="addPrice">
+                                        <label :for="feature.id" class="m-0 text-capitalize d-flex " style="gap:30px"> <span>
+                                            {{ feature.name }}
+                                        </span> <span>${{ feature.price }}</span> </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="description mt-4">
+                                
+                                <h4 class="text-uppercase text-dark font-weight-bold"> about {{ product.name }} </h4>
+                                <p class="small text-capitalize"> {{ product.description }} </p>
+                            </div>
+
+                            <div class="d-flex align-items-center mt-4" style="gap:30px">
+                                <div>
+                                    <h1 style="color: var(--primary-color)">${{ product.price }} </h1>
+                                </div>
+                                <div v-show="addItem" role="button" class="add-to-cart shadow-lg" @click="addToCart"> 
+                                    <span class="material-icons" style="font-size:14px">
+                                        shopping_cart
+                                    </span>
+                                   <span>
+                                        Add to Cart
+                                   </span>
+                                   
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    <section class="ratings--reviews bg-white py-4">
+                                <div class="ratings ">
+                                    <h3 v-if="reviews">{{ reviews.length }} Reviews</h3>
+                                </div>
+
+                                <div class="mt-3">
+                                    <div class="">
+                                        <h4> Reviews</h4>
+                                    </div>
+                                    <div v-if="posting">
+                                        <p class="text-danger">Posting...</p>
+                                    </div>
+                                    <div  v-if="reviews.length === 0 " class="mt-3">
+                                        <span  class="cancelled">No Reviews for this product yet</span>
+                                    </div>
+                                    <div v-else >
+                                        <div v-for="review in reviews" class="d-flex mt-3" style="gap:20px" :key="review.id">
+                                            <div>
+                                                <span class="b user--avatar" :class="[review.reviewer_name.charAt(0)]">
+                                                    {{ review.reviewer_name.charAt(0) }}
+                                                </span>
+                                            </div>
+                                            <div class="">
+                                                <h5> {{ review.reviewer_name }} </h5>
+                                                <p class="small"> {{ review.comment }} </p>
+                                                <small class="text-secondary">Posted {{ timeStamp(review.created_at) }} </small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- Add Review  -->
+                                    <!-- <div class="mt-4" v-show="loggedIn">
+                                        
+                                        <form action="" @submit.prevent="addReview">
+                                            <div class="add--review">
+                                                <input type="text" v-model="comment" placeholder="Enter a Review">
+                                                <button type="submit" style="background-color: transparent; border:none">
+                                                    <IconComponent icon="akar-icons:send" style="font-size:30px"/>
+                                                </button>
+                                            </div>
+                                        </form>
+                                        
+                                    </div> -->
+                                </div>
+                            </section>
+                </section>
+            </div>
+        </div>
+
+        <div class="container">
+            <!-- Feature Products  -->
+            <div class="featured--products mb-4 container">
+                <div class="mt-4">
+                <h2 class="mb-4">Related Services</h2>
+                <div class="content">
+                    <div v-for="product in products.data" :key="product.id">
+                        <div class="creative--services creative--services1" :style="{ 'background-image': `url(${url}/services/photos/${product.app_icon})` }" role="button" @click="viewProduct(product.slug)">
+                            <div>
+                                <hr class="bg-white w-50" style="height:2px">
+                                <h3 class="text-capitalize"> {{ product.name }} </h3>
+                                <h5>Start at ${{product.price}}</h5>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                <router-link to="/categories" class="d-flex align-items-center mt-4 text-dark font-weight-bold" style="gap:20px"> <span>All Services</span>  <span class="material-icons">
+                arrow_forward
+                </span></router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { nairaFilter, percentFilter, percentageFilter, timeStamp } from '@/plugins/filters.js'
+import Gallery from '@/components/galleryView.vue'
+// import Plans from '@/components/plansView.vue'
+export default {
+    components:{
+        Gallery
+    },
+     data(){
+        return {
+            nairaFilter, percentFilter, percentageFilter, timeStamp,
+            addItem: false,
+            product: {},
+            slug: this.$route.params.slug,
+            item_id: '',
+            dataObj: {},
+            plansObj: [],
+            comment: '',
+            reviews: '',
+            posting: false,
+            rating: '',
+            cart: [],
+            cartItem: [],
+            selected_plan: '',
+            isActive: true,
+            plan_id: '',
+            feature_id: '',
+            total_amount:'',
+            products: [],
+            url: 'https://api.risingwork.com/',
+        }
+    },
+    methods:{
+        selectPlan(plan){
+            this.isActive = ( this.isActive === plan.id ) ? null : plan.id;
+            this.addItem = true;
+            console.log(plan);
+            this.plan_id = plan.id
+            this.selected_plan = plan.price;
+            console.log(this.selected_plan);
+            this.addPrice()
+        },
+        async getTrending(){
+            try {
+                let res = await this.$http.get('/trending-products') 
+                this.products = res.data.trending_products
+                console.log(res.data.trending_products);
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        addPrice(){
+            console.log(this.cartItem);
+                let totalPrice = this.cartItem.reduce((accumulator, item) => {
+                    return accumulator + item.price;
+                }, 0);
+
+            this.product.price = totalPrice + this.selected_plan;
+            this.total_amount = totalPrice + this.selected_plan;
+            console.log(totalPrice);
+        },
+        async addItemToCart(){
+            // var sub_array = [];
+            for (let item of this.cartItem) {
+                // sub_array.push(item.id);
+                this.feature_id = item.id
+                // super_array.push(sub_array.slice(0));
+            }
+            let formData = new FormData()
+            formData.append("product_id", this.product.id)
+            formData.append("plan_id", this.plan_id)
+            formData.append("product_name", this.product.name)
+            formData.append("feature_id", this.feature_id)
+            formData.append("total_amount", Number(this.total_amount))
+            try {
+                let res = await this.$axios.post('/add-to-cart', formData)
+                console.log(res);
+                this.$router.push('/cart/checkout')
+            } catch (error) {
+                console.log(error);
+                this.$toastify({
+                text: `Select only one feature please`,
+                className: "info",
+                style: {
+                    background: "red",
+                },
+                }).showToast();
+            }
+        },
+        async addToCart(){
+            if(!this.$store.getters.isLoggedIn){
+                for (let item of this.cartItem) {
+                // sub_array.push(item.id);
+                this.feature_id = item.id
+                // super_array.push(sub_array.slice(0));
+            }
+                let payload = {};
+                payload = {product_id: this.product.id, plan_id: this.plan_id, product_name: this.product.name, feature_id: this.feature_id, total_amount: this.total_amount}
+                console.log(payload);
+                // this.$router.push({ name: 'login' })
+                this.$store.dispatch("addCart", { payload });
+                let url = '/cart/checkout'
+                this.$router.push({ name: 'login', query: {return_url: url } });
+                
+            }
+            else {
+                this.addItemToCart();
+            }
+        },
+        async getProductId(){
+            let id = this.$route.query.id
+            if (id !== '') {
+                try {
+                let res = await this.$http.get(`/show-product/${id}`)
+                this.item_id = res.data.product
+                console.log(this.item_id);
+                this.product = res.data.product
+                this.rating = res.data
+                this.dataObj = res.data.product
+                this.reviews = res.data.product.reviews;
+                this.plansObj = res.data.product.plans
+                // this.getProduct()
+            } catch (error) {
+                console.log(error);
+            }
+            }
+            else{
+                try {
+                let res = await this.$http.get(`/show-product/${this.slug}`)
+                this.item_id = res.data.product
+                console.log(this.item_id);
+                this.product = res.data.product
+                this.rating = res.data
+                this.dataObj = res.data.product
+                this.reviews = res.data.product.reviews;
+                this.plansObj = res.data.product.plans
+                // this.getProduct()
+            } catch (error) {
+                console.log(error);
+            }
+            }
+        },
+       
+        async addReview(){
+            this.posting = true;
+            let dataObj = {
+                comment: this.comment,
+                product_id: this.item_id
+            }
+            try {
+                let res = await this.$axios.post('/create-review/', dataObj)
+                console.log(res);
+            } catch (error) {
+                console.log(error);
+            }
+            this.posting = false;
+            this.comment = "";
+            // this.getProductId();
+            this.getProduct();
+        }
+    },
+    mounted(){
+            this.getProductId()
+            this.getTrending()
+    },
+    computed:{
+      loggedIn(){
+        return this.$store.getters.isLoggedIn
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
